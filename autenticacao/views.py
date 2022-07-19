9a81da4989d084b5d071a0c453206c323d2cfa98
Gabriel M. N <g.magalhaes.dev@gmail.com>
2022-07-19 17:55:28 -0300

    página de registro completa v1.0

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,12 +1,17 @@
+from asyncio import constants
 from .utils import password_is_valid
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 # Create your views here.
 def register(request):
     if request.method == 'GET': 
         return render(request, 'register.html')
+
     if request.method == 'POST':
         usuario = request.POST.get('usuario')
         email = request.POST.get('email')
@@ -14,8 +19,19 @@ def register(request):
         confSenha = request.POST.get('confirm_password')
         if not password_is_valid(request, senha, confSenha):
             return redirect("/auth/register")
-        return HttpResponse(f'Seja bem vindo {usuario}')
-        
-        
+    try:
+        user = User.objects.create_user(
+            username = usuario,
+            password = senha,
+            is_active = False
+        )
+        user.save()
+        messages.add_message(request, constants.SUCCESS, 'Cadastro efetuado, verifique sua caixa de email para confirmar')
+        return redirect("/auth/register")
+
+    except:
+        messages.add_message(request, constants.ERROR, 'Erro do sistema')
+        return redirect("/auth/register")
+
 def login(request):
     return HttpResponse('Bem vindo, faça login')
--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+from email.message import Message
 from pathlib import Path
 import os
 
@@ -126,3 +127,15 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Messages
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.DEBUG : 'alert-primary',
+    constants.ERROR : 'alert-danger',
+    constants.SUCCESS : 'alert-success',
+    constants.INFO : 'alert-infor',
+    constants.WARNING : 'alert-warning'
+}
